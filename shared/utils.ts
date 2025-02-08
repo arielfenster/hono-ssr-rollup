761c10b2d63f36d1c10ab0d591884d4eb8d6450a
Ariel Fenster <arielfenster1610@gmail.com>
2025-02-08 23:16:07 +0200

    added prod domain for assetss

--- shared/utils.ts
+++ shared/utils.ts
@@ -1,6 +1,8 @@
 import { STATIC_JS_PATH, type AppPage, STATIC_IMAGE_PATH } from './constants';
 
-const DOMAIN = 'http://localhost:3000';
+const IS_PROD = process.env.NODE_ENV === 'production';
+
+const DOMAIN = IS_PROD ? 'https://hono-ssr-test.onrender.com/' : 'http://localhost:3000';
 
 export function getPageScript(page: AppPage) {
 	return `${DOMAIN}/${STATIC_JS_PATH}/${page}.js`;
